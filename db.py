31c154c501de9fee8870ec04e67631d467338c25
Thiago RSL <thiagors.leao@gmail.com>
2025-03-07 17:25:44 -0300

    Added db functions

--- db.py
+++ db.py
@@ -31,5 +31,26 @@ class ElevatorDemand(baseDec):
     state_id = Column(Integer, ForeignKey('states.id')) #Reference to the state it when it got this demand
     demanded_floor = Column(Integer)
 
+
+def NewState(e_id, cf, tf, date, moving, vacant):
+
+    return ElevatorState(
+        elevator_id = e_id,
+        current_floor = cf,
+        target_floor = tf,
+        date = date,
+        moving = moving,
+        vacant = vacant
+    )
+
+def NewDemand(e_id, state_id, demanded_at, floor):
+
+    return ElevatorDemand(
+        elevator_id = e_id,
+        date = demanded_at,
+        starting_state_id = state_id,
+        goal_floor = floor
+    )
+
 if __name__ == "__main__":
     baseDec.metadata.create_all(engine)
